4b3988e61f47b04cbac8691174657110df7eb799
himam <himajmadhavan@lewisu.edu>
2022-09-09 19:27:47 -0500

    Added to java program, one named DataTypeExample and
    DataTypeExamplePart2

---
+++ MyPracticeCode/src/DataTypeExample/App.java
@@ -0,0 +1,7 @@
+package DataTypeExample;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello it is Fridayt");
+	}
+}
---
+++ MyPracticeCode/src/DataTypeExamplePart2/App.java
@@ -0,0 +1,7 @@
+package DataTypeExamplePart2;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("This is a trial part two ");
+	}
+}
