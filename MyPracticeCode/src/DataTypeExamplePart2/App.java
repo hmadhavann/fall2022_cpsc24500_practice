68517365cd208fbba664cec2c538d4932a945736
hmadhavann <90738791+hmadhavann@users.noreply.github.com>
2022-09-09 19:32:06 -0500

    Made change using github editor to
    to DataTypeExamplePart2

--- MyPracticeCode/src/DataTypeExamplePart2/App.java
+++ MyPracticeCode/src/DataTypeExamplePart2/App.java
@@ -3,5 +3,6 @@ package DataTypeExamplePart2;
 public class App {
 	public static void main(String[] args) {
 		System.out.println("This is a trial part two ");
+		System.out.println("This change was made on the github website");
 	}
 }
